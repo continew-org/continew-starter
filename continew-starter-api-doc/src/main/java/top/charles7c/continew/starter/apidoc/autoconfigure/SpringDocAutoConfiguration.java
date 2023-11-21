7973f6c2dc7dc5defdec16bd3eb7885ecb593554
Charles7c <charles7c@126.com>
2023-11-21 22:05:43 +0800

    feat: 新增 API 文档（Knife4j：Spring Doc）自动配置

---
+++ continew-starter-api-doc/src/main/java/top/charles7c/continew/starter/apidoc/autoconfigure/SpringDocAutoConfiguration.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2022-present Charles7c Authors. All Rights Reserved.
+ * <p>
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl.html
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.charles7c.continew.starter.apidoc.autoconfigure;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import top.charles7c.continew.starter.core.autoconfigure.ProjectProperties;
+
+/**
+ * API 文档自动配置
+ *
+ * @author Charles7c
+ * @since 1.0.0
+ */
+@Slf4j
+@Lazy
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnProperty(name = "springdoc.swagger-ui.enabled", havingValue = "true")
+public class SpringDocAutoConfiguration {
+
+    /**
+     * Open API 配置
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public OpenAPI openApi(ProjectProperties properties) {
+        Info info = new Info()
+                .title(String.format("%s %s", properties.getName(), "API 文档"))
+                .version(properties.getVersion())
+                .description(properties.getDescription());
+        ProjectProperties.Contact contact = properties.getContact();
+        if (null != contact) {
+            info.contact(new Contact().name(contact.getName())
+                    .email(contact.getEmail())
+                    .url(contact.getUrl()));
+        }
+        ProjectProperties.License license = properties.getLicense();
+        if (null != license) {
+            info.license(new License().name(license.getName())
+                    .url(license.getUrl()));
+        }
+        return new OpenAPI().info(info);
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        log.info("[ContiNew Starter] - Auto Configuration 'ApiDoc' completed initialization.");
+    }
+}
---
+++ continew-starter-core/src/main/java/top/charles7c/continew/starter/core/autoconfigure/ProjectProperties.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2022-present Charles7c Authors. All Rights Reserved.
+ * <p>
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl.html
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.charles7c.continew.starter.core.autoconfigure;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.stereotype.Component;
+
+/**
+ * 项目配置属性
+ *
+ * @author Charles7c
+ * @since 1.0.0
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "project")
+public class ProjectProperties {
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 应用名称
+     */
+    private String appName;
+
+    /**
+     * 版本
+     */
+    private String version;
+
+    /**
+     * 描述
+     */
+    private String description;
+
+    /**
+     * URL
+     */
+    private String url;
+
+    /**
+     * 基本包
+     */
+    private String basePackage;
+
+    /**
+     * 联系人
+     */
+    @NestedConfigurationProperty
+    private Contact contact;
+
+    /**
+     * 许可协议
+     */
+    @NestedConfigurationProperty
+    private License license;
+
+    /**
+     * 是否为生产环境
+     */
+    private boolean production = false;
+
+    /**
+     * 联系人配置属性
+     */
+    @Data
+    public static class Contact {
+        /**
+         * 名称
+         */
+        private String name;
+
+        /**
+         * 邮箱
+         */
+        private String email;
+
+        /**
+         * URL
+         */
+        private String url;
+    }
+
+    /**
+     * 许可协议配置属性
+     */
+    @Data
+    public static class License {
+        /**
+         * 名称
+         */
+        private String name;
+
+        /**
+         * URL
+         */
+        private String url;
+    }
+}
