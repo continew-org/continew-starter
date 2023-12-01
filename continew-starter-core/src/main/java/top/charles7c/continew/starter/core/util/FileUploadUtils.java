ed961db06f4e85ca2880bddffeb6a0e964916ad3
Charles7c <charles7c@126.com>
2023-12-01 21:32:00 +0800

    feat: 新增常用工具类

---
+++ continew-starter-core/src/main/java/top/charles7c/continew/starter/core/util/FileUploadUtils.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2022-present Charles7c Authors. All Rights Reserved.
+ * <p>
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl.html
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.charles7c.continew.starter.core.util;
+
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.io.file.FileNameUtil;
+import cn.hutool.core.util.IdUtil;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.time.LocalDateTime;
+
+/**
+ * 文件上传工具类
+ *
+ * @author Zheng Jie（<a href="https://gitee.com/elunez/eladmin">ELADMIN</a>）
+ * @author Charles7c
+ * @since 1.0.0
+ */
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class FileUploadUtils {
+
+    /**
+     * 上传文件
+     *
+     * @param multipartFile          源文件对象
+     * @param filePath               文件路径
+     * @param isKeepOriginalFilename 是否保留原文件名
+     * @return 目标文件对象
+     */
+    public static File upload(MultipartFile multipartFile, String filePath, boolean isKeepOriginalFilename) {
+        String originalFilename = multipartFile.getOriginalFilename();
+        String extensionName = FileNameUtil.extName(originalFilename);
+
+        String fileName;
+        if (isKeepOriginalFilename) {
+            fileName = String.format("%s-%s.%s", FileNameUtil.getPrefix(originalFilename),
+                    DateUtil.format(LocalDateTime.now(), DatePattern.PURE_DATETIME_MS_PATTERN), extensionName);
+        } else {
+            fileName = String.format("%s.%s", IdUtil.fastSimpleUUID(), extensionName);
+        }
+
+        try {
+            String pathname = filePath + fileName;
+            File dest = new File(pathname).getCanonicalFile();
+            // 如果父路径不存在，自动创建
+            if (!dest.getParentFile().exists()) {
+                if (!dest.getParentFile().mkdirs()) {
+                    log.error("Create upload file parent path failed.");
+                }
+            }
+            // 文件写入
+            multipartFile.transferTo(dest);
+            return dest;
+        } catch (Exception e) {
+            log.error("Upload file occurred an error: {}. fileName: {}.", e.getMessage(), fileName, e);
+        }
+        return null;
+    }
+}
