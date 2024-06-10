aa50e2e76363031a88047d0c4a0c7e4b7bc55766
hxlcw <hx13265948041@163.com>
2024-06-10 20:56:26 +0800

    test(security-crypto): 添加【getById、mpUpdate】测试方法

---
+++ continew-starter-security/continew-starter-security-crypto/src/test/java/top/continew/starter/security/crypto/domain/TestUser.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2022-present Charles7c Authors. All Rights Reserved.
+ * <p>
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl.html
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.continew.starter.security.crypto.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import top.continew.starter.security.crypto.annotation.FieldEncrypt;
+
+/**
+ * @author huangxiong
+ * @date 2024/06/06 22:55
+ */
+@TableName("t_user")
+public class TestUser {
+
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    @FieldEncrypt
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "TestUser{" + "id=" + id + ", name='" + name + '\'' + ", age=" + age + ", email='" + email + '\'' + '}';
+    }
+
+}
---
+++ continew-starter-security/continew-starter-security-crypto/src/test/java/top/continew/starter/security/crypto/mapper/TestUserMapper.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2022-present Charles7c Authors. All Rights Reserved.
+ * <p>
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl.html
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.continew.starter.security.crypto.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import top.continew.starter.security.crypto.domain.TestUser;
+
+/**
+ * @author huangxiong
+ * @date 2024/06/06 22:58
+ */
+@Mapper
+public interface TestUserMapper extends BaseMapper<TestUser> {
+
+    @Select("select email from t_user where id = #{id} ")
+    public String getRawEmail(@Param("id") Integer id);
+}
