0b8a27178fac9579c216e37a5fa36327b7bd63a9
HuangXiong <hx13265948041@163.com>
2024-06-12 08:54:49 +0800

    chore(security-crypto): 移除测试用例和POM中的测试依赖

--- continew-starter-security/continew-starter-security-crypto/src/test/resources/db/data.sql
+++
@@ -1,6 +0,0 @@
-delete from t_user;
-INSERT INTO t_user (name, age, email) VALUES ('John', 30, 'john.doe@example.com');
-INSERT INTO t_user (name, age, email) VALUES ('Jack', 20, 'test2@baomidou.com');
-INSERT INTO t_user (name, age, email) VALUES ('Tom', 28, 'test3@baomidou.com');
-INSERT INTO t_user (name, age, email) VALUES ('Sandy', 21, 'test4@baomidou.com');
-INSERT INTO t_user (name, age, email) VALUES ('Billie', 24, 'test5@baomidou.com');
