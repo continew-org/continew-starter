2208dbd0282964aab76b02e811d6689ba69d75fd
Charles7c <charles7c@126.com>
2024-09-05 19:54:19 +0800

    chore: 升级依赖 graceful-response 4.0.1-boot3 => 5.0.0-boot3

---
+++ continew-starter-web/src/main/java/top/continew/starter/web/autoconfigure/response/DefaultBeforeControllerAdviceProcessImpl.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2022-present Charles7c Authors. All Rights Reserved.
+ * <p>
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl.html
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.continew.starter.web.autoconfigure.response;
+
+import com.feiniaojin.gracefulresponse.advice.lifecycle.exception.BeforeControllerAdviceProcess;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import top.continew.starter.web.util.SpringWebUtils;
+
+/**
+ * 默认回调处理器实现
+ *
+ * @author Charles7c
+ * @since 2.6.0
+ */
+public class DefaultBeforeControllerAdviceProcessImpl implements BeforeControllerAdviceProcess {
+
+    private final Logger log = LoggerFactory.getLogger(DefaultBeforeControllerAdviceProcessImpl.class);
+    private final GlobalResponseProperties globalResponseProperties;
+
+    public DefaultBeforeControllerAdviceProcessImpl(GlobalResponseProperties globalResponseProperties) {
+        this.globalResponseProperties = globalResponseProperties;
+    }
+
+    @Override
+    public void call(Throwable throwable) {
+        if (globalResponseProperties.isPrintExceptionInGlobalAdvice()) {
+            HttpServletRequest request = SpringWebUtils.getRequest();
+            log.error("[{}] {}", request.getMethod(), request.getRequestURI(), throwable);
+        }
+    }
+}
--- continew-starter-web/src/main/java/top/continew/starter/web/autoconfigure/response/GlobalResponseAutoConfiguration.java
+++ continew-starter-web/src/main/java/top/continew/starter/web/autoconfigure/response/GlobalResponseAutoConfiguration.java
@@ -18,6 +18,9 @@ package top.continew.starter.web.autoconfigure.response;
 
 import com.feiniaojin.gracefulresponse.ExceptionAliasRegister;
 import com.feiniaojin.gracefulresponse.advice.*;
+import com.feiniaojin.gracefulresponse.advice.lifecycle.exception.BeforeControllerAdviceProcess;
+import com.feiniaojin.gracefulresponse.advice.lifecycle.exception.ControllerAdvicePredicate;
+import com.feiniaojin.gracefulresponse.advice.lifecycle.response.ResponseBodyAdvicePredicate;
 import com.feiniaojin.gracefulresponse.api.ResponseFactory;
 import com.feiniaojin.gracefulresponse.api.ResponseStatusFactory;
 import com.feiniaojin.gracefulresponse.defaults.DefaultResponseFactory;
@@ -37,6 +40,7 @@ import top.continew.starter.core.constant.PropertiesConstants;
 import top.continew.starter.core.util.GeneralPropertySourceFactory;
 
 import java.util.Locale;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * 全局响应自动配置
@@ -57,30 +61,17 @@ public class GlobalResponseAutoConfiguration {
     }
 
     /**
-     * 全局异常处理
-     */
-    @Bean
-    @ConditionalOnMissingBean
-    public GrGlobalExceptionAdvice globalExceptionAdvice() {
-        return new GrGlobalExceptionAdvice();
-    }
-
-    /**
-     * 全局校验异常处理
-     */
-    @Bean
-    @ConditionalOnMissingBean
-    public GrValidationExceptionAdvice validationExceptionAdvice() {
-        return new GrValidationExceptionAdvice();
-    }
-
-    /**
      * 全局响应体处理（非 void）
      */
     @Bean
     @ConditionalOnMissingBean
-    public GrNotVoidResponseBodyAdvice notVoidResponseBodyAdvice() {
-        return new GrNotVoidResponseBodyAdvice();
+    public GrNotVoidResponseBodyAdvice grNotVoidResponseBodyAdvice() {
+        GrNotVoidResponseBodyAdvice notVoidResponseBodyAdvice = new GrNotVoidResponseBodyAdvice();
+        CopyOnWriteArrayList<ResponseBodyAdvicePredicate> copyOnWriteArrayList = new CopyOnWriteArrayList<>();
+        copyOnWriteArrayList.add(notVoidResponseBodyAdvice);
+        notVoidResponseBodyAdvice.setPredicates(copyOnWriteArrayList);
+        notVoidResponseBodyAdvice.setResponseBodyAdviceProcessor(notVoidResponseBodyAdvice);
+        return notVoidResponseBodyAdvice;
     }
 
     /**
@@ -88,8 +79,104 @@ public class GlobalResponseAutoConfiguration {
      */
     @Bean
     @ConditionalOnMissingBean
-    public GrVoidResponseBodyAdvice voidResponseBodyAdvice() {
-        return new GrVoidResponseBodyAdvice();
+    public GrVoidResponseBodyAdvice grVoidResponseBodyAdvice() {
+        GrVoidResponseBodyAdvice voidResponseBodyAdvice = new GrVoidResponseBodyAdvice();
+        CopyOnWriteArrayList<ResponseBodyAdvicePredicate> copyOnWriteArrayList = new CopyOnWriteArrayList<>();
+        copyOnWriteArrayList.add(voidResponseBodyAdvice);
+        voidResponseBodyAdvice.setPredicates(copyOnWriteArrayList);
+        voidResponseBodyAdvice.setResponseBodyAdviceProcessor(voidResponseBodyAdvice);
+        return voidResponseBodyAdvice;
+    }
+
+    /**
+     * 处理前回调（目前仅打印异常日志）
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public BeforeControllerAdviceProcess beforeControllerAdviceProcess() {
+        return new DefaultBeforeControllerAdviceProcessImpl(globalResponseProperties);
+    }
+
+    /**
+     * 框架异常处理器
+     */
+    @Bean
+    public FrameworkExceptionAdvice frameworkExceptionAdvice(BeforeControllerAdviceProcess beforeControllerAdviceProcess) {
+        FrameworkExceptionAdvice frameworkExceptionAdvice = new FrameworkExceptionAdvice();
+        frameworkExceptionAdvice.setRejectStrategy(new DefaultRejectStrategyImpl());
+        frameworkExceptionAdvice.setControllerAdviceProcessor(frameworkExceptionAdvice);
+        frameworkExceptionAdvice.setBeforeControllerAdviceProcess(beforeControllerAdviceProcess);
+        frameworkExceptionAdvice.setControllerAdviceHttpProcessor(frameworkExceptionAdvice);
+        return frameworkExceptionAdvice;
+    }
+
+    /**
+     * 数据校验异常处理器
+     */
+    @Bean
+    public DataExceptionAdvice dataExceptionAdvice(BeforeControllerAdviceProcess beforeControllerAdviceProcess) {
+        DataExceptionAdvice dataExceptionAdvice = new DataExceptionAdvice();
+        dataExceptionAdvice.setRejectStrategy(new DefaultRejectStrategyImpl());
+        dataExceptionAdvice.setControllerAdviceProcessor(dataExceptionAdvice);
+        dataExceptionAdvice.setBeforeControllerAdviceProcess(beforeControllerAdviceProcess);
+        dataExceptionAdvice.setControllerAdviceHttpProcessor(dataExceptionAdvice);
+        return dataExceptionAdvice;
+    }
+
+    /**
+     * 默认全局异常处理器
+     */
+    @Bean
+    public DefaultGlobalExceptionAdvice defaultGlobalExceptionAdvice(BeforeControllerAdviceProcess beforeControllerAdviceProcess) {
+        DefaultGlobalExceptionAdvice advice = new DefaultGlobalExceptionAdvice();
+        advice.setRejectStrategy(new DefaultRejectStrategyImpl());
+        CopyOnWriteArrayList<ControllerAdvicePredicate> copyOnWriteArrayList = new CopyOnWriteArrayList<>();
+        copyOnWriteArrayList.add(advice);
+        advice.setPredicates(copyOnWriteArrayList);
+        advice.setControllerAdviceProcessor(advice);
+        advice.setBeforeControllerAdviceProcess(beforeControllerAdviceProcess);
+        advice.setControllerAdviceHttpProcessor(advice);
+        return advice;
+    }
+
+    /**
+     * 默认参数校验异常处理器
+     */
+    @Bean
+    public DefaultValidationExceptionAdvice defaultValidationExceptionAdvice(BeforeControllerAdviceProcess beforeControllerAdviceProcess) {
+        DefaultValidationExceptionAdvice advice = new DefaultValidationExceptionAdvice();
+        advice.setRejectStrategy(new DefaultRejectStrategyImpl());
+        advice.setControllerAdviceProcessor(advice);
+        advice.setBeforeControllerAdviceProcess(beforeControllerAdviceProcess);
+        advice.setControllerAdviceHttpProcessor(advice);
+        return advice;
+    }
+
+    /**
+     * 国际化支持
+     */
+    @Bean
+    @ConditionalOnProperty(prefix = PropertiesConstants.WEB_RESPONSE, name = "i18n", havingValue = "true")
+    public GrI18nResponseBodyAdvice grI18nResponseBodyAdvice() {
+        GrI18nResponseBodyAdvice i18nResponseBodyAdvice = new GrI18nResponseBodyAdvice();
+        CopyOnWriteArrayList<ResponseBodyAdvicePredicate> copyOnWriteArrayList = new CopyOnWriteArrayList<>();
+        copyOnWriteArrayList.add(i18nResponseBodyAdvice);
+        i18nResponseBodyAdvice.setPredicates(copyOnWriteArrayList);
+        i18nResponseBodyAdvice.setResponseBodyAdviceProcessor(i18nResponseBodyAdvice);
+        return i18nResponseBodyAdvice;
+    }
+
+    /**
+     * 国际化配置
+     */
+    @Bean
+    @ConditionalOnProperty(prefix = PropertiesConstants.WEB_RESPONSE, name = "i18n", havingValue = "true")
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("i18n", "i18n/empty-messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setDefaultLocale(Locale.CHINA);
+        return messageSource;
     }
 
     /**
@@ -124,28 +211,6 @@ public class GlobalResponseAutoConfiguration {
     @Bean
     public AdviceSupport adviceSupport() {
         return new AdviceSupport();
-    }
-
-    /**
-     * 国际化支持
-     */
-    @Bean
-    @ConditionalOnProperty(prefix = PropertiesConstants.WEB_RESPONSE, name = "i18n", havingValue = "true")
-    public GrI18nAdvice i18nAdvice() {
-        return new GrI18nAdvice();
-    }
-
-    /**
-     * 国际化配置
-     */
-    @Bean
-    @ConditionalOnProperty(prefix = PropertiesConstants.WEB_RESPONSE, name = "i18n", havingValue = "true")
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasenames("i18n", "i18n/empty-messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setDefaultLocale(Locale.CHINA);
-        return messageSource;
     }
 
     /**
