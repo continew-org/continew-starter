be4dec5a3039625e62d346dbb148206b602af6aa
Charles7c <charles7c@126.com>
2024-09-24 22:14:50 +0800

    fix(log/interceptor): 修复全局配置和局部配置包含请求、响应体冲突

--- continew-starter-log/continew-starter-log-interceptor/src/main/java/top/continew/starter/log/interceptor/handler/LogFilter.java
+++ continew-starter-log/continew-starter-log-interceptor/src/main/java/top/continew/starter/log/interceptor/handler/LogFilter.java
@@ -28,14 +28,12 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 import org.springframework.web.util.WebUtils;
-import top.continew.starter.log.core.enums.Include;
 import top.continew.starter.log.interceptor.autoconfigure.LogProperties;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Objects;
-import java.util.Set;
 
 /**
  * 日志过滤器
@@ -123,9 +121,7 @@ public class LogFilter extends OncePerRequestFilter implements Ordered {
      * @return true：是；false：否
      */
     private boolean isRequestWrapper(HttpServletRequest request) {
-        Set<Include> includeSet = logProperties.getIncludes();
-        return !(request instanceof ContentCachingRequestWrapper) && (includeSet
-            .contains(Include.REQUEST_BODY) || includeSet.contains(Include.REQUEST_PARAM));
+        return !(request instanceof ContentCachingRequestWrapper);
     }
 
     /**
@@ -135,9 +131,7 @@ public class LogFilter extends OncePerRequestFilter implements Ordered {
      * @return true：是；false：否
      */
     private boolean isResponseWrapper(HttpServletResponse response) {
-        Set<Include> includeSet = logProperties.getIncludes();
-        return !(response instanceof ContentCachingResponseWrapper) && (includeSet
-            .contains(Include.RESPONSE_BODY) || includeSet.contains(Include.RESPONSE_PARAM));
+        return !(response instanceof ContentCachingResponseWrapper);
     }
 
     /**
