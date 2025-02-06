5309310a41bb6c9433fe5f779231292a05413dc7
luoqiz <luoqiangzheng@gmail.com>
2025-02-06 15:32:33 +0800

    feat: 添加敏感词模块

---
+++ continew-starter-sensitive-words/src/main/java/top/continew/starter/sensitive/words/service/DefaultSensitiveWordsService.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2022-present Charles7c Authors. All Rights Reserved.
+ * <p>
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl.html
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package top.continew.starter.sensitive.words.service;
+
+import cn.hutool.dfa.WordTree;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 默认敏感词服务
+ */
+@Component
+@ConditionalOnBean(SensitiveWordsConfig.class)
+@ConditionalOnMissingBean(SensitiveWordsService.class)
+public class DefaultSensitiveWordsService implements SensitiveWordsService {
+
+    private final SensitiveWordsConfig sensitiveWordsConfig;
+
+    private WordTree tree = new WordTree();
+
+    public DefaultSensitiveWordsService(SensitiveWordsConfig sensitiveWordsConfig) {
+        this.sensitiveWordsConfig = sensitiveWordsConfig;
+        if (sensitiveWordsConfig != null && sensitiveWordsConfig.getWords() != null) {
+            tree.addWords(sensitiveWordsConfig.getWords());
+        }
+    }
+
+    @Override
+    public List<String> check(String content) {
+        return tree.matchAll(content, -1, false, true);
+    }
+}
