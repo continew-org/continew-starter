6099ae10c77c33b6fd176f316a711a522c4d0522
Charles7c <charles7c@126.com>
2026-08-02 23:22:22 +0800

    build(dependencies): 更新项目依赖版本，spring-boot.version 3.5.13 => 3.5.16
    spring-cloud.version 2025.0.1 => 2025.0.3
    jetcache.version 2.7.8 => 2.7.9
    mybatis-plus.version 3.5.16 => 3.5.17（CrudRepository => IRepository）
    mybatis-flex.version 1.11.6 => 1.11.8
    cosid.version 2.13.3 => 2.15.2
    snail-job.version 1.9.0 => 1.10.0
    aws-sdk.version 2.42.24 => 2.42.41
    aws-crt.version 0.44.0 => 0.48.3
    crane4j.version 2.9.0 => 2.10.0
    nextdoc4j.version 1.2.0 => 1.4.1（BOM 依赖变更）
    swagger.version 2.2.45 => 2.2.52
    hutool.version 5.8.44 => 5.8.47
    commons-io.version 2.21.0 => 2.22.0
    flatten.version 1.7.3 => 1.8.0
    spotless.version 3.0.0 => 3.9.0
    sonar.version 5.2.0.4988 => 5.7.0.6970

--- continew-starter-data/continew-starter-data-mp/src/main/java/top/continew/starter/data/service/impl/ServiceImpl.java
+++ continew-starter-data/continew-starter-data-mp/src/main/java/top/continew/starter/data/service/impl/ServiceImpl.java
@@ -26,7 +26,6 @@ import com.baomidou.mybatisplus.core.metadata.TableInfoHelper;
 import com.baomidou.mybatisplus.core.toolkit.*;
 import com.baomidou.mybatisplus.core.toolkit.reflect.GenericTypeUtils;
 import com.baomidou.mybatisplus.extension.repository.AbstractRepository;
-import com.baomidou.mybatisplus.extension.repository.CrudRepository;
 import com.baomidou.mybatisplus.extension.repository.IRepository;
 import com.baomidou.mybatisplus.extension.toolkit.SqlHelper;
 import org.apache.ibatis.binding.MapperMethod;
@@ -46,15 +45,15 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 /**
  * 通用业务实现类
  *
- * <p>将 MP 的 {@link CrudRepository} 迁移至本类中，减少两层继承，解决层级过多出现 Sonar 警告的问题</p>
+ * <p>将 MP 的 {@link IRepository} 迁移至本类中，减少两层继承，解决层级过多出现 Sonar 警告的问题</p>
  *
- * @see CrudRepository
+ * @see IRepository
  *
  * @param <M> Mapper 接口
  * @param <T> 实体类型
@@ -143,9 +142,8 @@ public abstract class ServiceImpl<M extends BaseMapper<T>, T> implements IServic
      * @param consumer  执行方法
      * @param <E>       泛型
      * @return 操作结果
-     * @see AbstractRepository#executeBatch(Collection, int, BiConsumer)
      */
-    protected <E> boolean executeBatch(Collection<E> list, int batchSize, BiConsumer<SqlSession, E> consumer) {
+    protected <E> boolean executeBatch(Collection<E> list, int batchSize, BiFunction<SqlSession, E, Integer> consumer) {
         return SqlHelper.executeBatch(getSqlSessionFactory(), this.innerLog, list, batchSize, consumer);
     }
 
@@ -156,9 +154,8 @@ public abstract class ServiceImpl<M extends BaseMapper<T>, T> implements IServic
      * @param consumer 执行方法
      * @param <E>      泛型
      * @return 操作结果
-     * @see AbstractRepository#executeBatch(Collection, BiConsumer)
      */
-    protected <E> boolean executeBatch(Collection<E> list, BiConsumer<SqlSession, E> consumer) {
+    protected <E> boolean executeBatch(Collection<E> list, BiFunction<SqlSession, E, Integer> consumer) {
         return executeBatch(list, DEFAULT_BATCH_SIZE, consumer);
     }
 
@@ -181,7 +178,7 @@ public abstract class ServiceImpl<M extends BaseMapper<T>, T> implements IServic
      * @param entityList 数据集合
      * @param batchSize  批量大小
      * @return boolean
-     * @see CrudRepository#saveBatch(Collection, int)
+     * @see IRepository#saveBatch(Collection, int)
      */
     @Transactional(rollbackFor = Exception.class)
     @Override
@@ -196,7 +193,7 @@ public abstract class ServiceImpl<M extends BaseMapper<T>, T> implements IServic
      * @param entityList 数据集合
      * @param batchSize  批量大小
      * @return boolean
-     * @see CrudRepository#saveOrUpdateBatch(Collection, int)
+     * @see IRepository#saveOrUpdateBatch(Collection, int)
      */
     @Transactional(rollbackFor = Exception.class)
     @Override
@@ -213,7 +210,7 @@ public abstract class ServiceImpl<M extends BaseMapper<T>, T> implements IServic
             }, (sqlSession, entity) -> {
                 MapperMethod.ParamMap<T> param = new MapperMethod.ParamMap<>();
                 param.put(Constants.ENTITY, entity);
-                sqlSession.update(getSqlStatement(SqlMethod.UPDATE_BY_ID), param);
+                return sqlSession.update(getSqlStatement(SqlMethod.UPDATE_BY_ID), param);
             });
     }
 
@@ -223,7 +220,7 @@ public abstract class ServiceImpl<M extends BaseMapper<T>, T> implements IServic
      * @param entityList 数据集合
      * @param batchSize  批量大小
      * @return boolean
-     * @see CrudRepository#updateBatchById(Collection, int)
+     * @see IRepository#updateBatchById(Collection, int)
      */
     @Transactional(rollbackFor = Exception.class)
     @Override
@@ -232,7 +229,7 @@ public abstract class ServiceImpl<M extends BaseMapper<T>, T> implements IServic
         return executeBatch(entityList, batchSize, (sqlSession, entity) -> {
             MapperMethod.ParamMap<T> param = new MapperMethod.ParamMap<>();
             param.put(Constants.ENTITY, entity);
-            sqlSession.update(sqlStatement, param);
+            return sqlSession.update(sqlStatement, param);
         });
     }
 
@@ -260,7 +257,6 @@ public abstract class ServiceImpl<M extends BaseMapper<T>, T> implements IServic
      * 获取当前 Mapper 类型
      *
      * @return 当前 Mapper 类型
-     * @see CrudRepository#getMapperClass()
      */
     public Class<M> getMapperClass() {
         if (this.mapperClass == null) {
@@ -286,7 +282,6 @@ public abstract class ServiceImpl<M extends BaseMapper<T>, T> implements IServic
      * 获取 SqlSessionFactory
      *
      * @return SqlSessionFactory
-     * @see AbstractRepository#getSqlSessionFactory()
      */
     protected SqlSessionFactory getSqlSessionFactory() {
         if (this.sqlSessionFactory == null) {
@@ -301,7 +296,6 @@ public abstract class ServiceImpl<M extends BaseMapper<T>, T> implements IServic
      *
      * @param sqlMethod 方法名
      * @return 命名id
-     * @see CrudRepository#getSqlStatement(SqlMethod)
      */
     protected String getSqlStatement(SqlMethod sqlMethod) {
         return SqlHelper.getSqlStatement(this.getMapperClass(), sqlMethod);
